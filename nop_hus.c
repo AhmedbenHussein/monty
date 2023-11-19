096a1cb5eab9286fcf7db7e9a56f54398ddf9b6b
AhmedbenHussein <ahmed.h.elrawi@gmail.com>
2023-11-19 03:30:28 -0800

    monty

---
+++ nop_hus.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ *  get_nop - When no operate presents
+ *  @line_number: Int of lines num
+ *  @stack: Head a stacks
+ *
+ *  Returns: Voidd
+ */
+
+void get_nop(stack_t **stack, unsigned int line_number)
+{
+(void) stack;
+(void) line_number;
+}
